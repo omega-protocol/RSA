6d4621001be596956edd3af0e82334f432d546e9
Tin Lam <tin@irrational.io>
2024-07-21 17:54:15 -0500

    merge  (#3)
    * update mages and shaman
    * update CHANGELOG
    
    ---------
    
    Signed-off-by: Tin Lam <tinlam@gmail.com>

--- SpellData/Shaman.lua
+++ SpellData/Shaman.lua
@@ -184,23 +184,8 @@ local defaults = {
 			},
 		},
 	},
-	['earthElemental'] = {
-		spellID = 198103,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] activated!",},
-			},
-			['SPELL_AURA_REMOVED'] = {
-				messages = {"[LINK] finished!",},
-			},
-		},
-	},
-    ['earthGrabTotem'] = {
-		spellID = 51485,
+	['counterstrikeTotem'] = {
+		spellID = 204331,
 		configDisplay = {
 			isDefault = true,
 			disabledChannels = {whisper = true},
@@ -214,89 +199,7 @@ local defaults = {
 			},
 		},
 	},
-	['groundingTotem'] = {
-		spellID = 204336,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] placed!",},
-			},
-			['SPELL_AURA_REMOVED'] = {
-				messages = {"[LINK] finished!",},
-			},
-		},
-	},
-	['healingTideTotem'] = {
-		spellID = 108280,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] placed!",},
-			},
-			['SPELL_AURA_REMOVED'] = {
-				messages = {"[LINK] finished!",},
-			},
-		},
-	},
-    ['poisonCleansingTotem'] = {
-		spellID = 383013,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] placed!",},
-			},
-			['SPELL_AURA_REMOVED'] = {
-				messages = {"[LINK] finished!",},
-			},
-			['SPELL_DISPEL'] = {
-				messages = {"Cleansed [TARGET]'s [EXTRALINK]!",},
-				tags = {
-					TARGET = true,
-					EXTRA = true,
-				},
-			},
-		},
-	},
-    ['spiritLinkTotem'] = {
-		spellID = 98008,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] placed!",},
-			},
-			['SPELL_AURA_REMOVED'] = {
-				messages = {"[LINK] finished!",},
-			},
-		},
-	},
-	['stoneskinTotem'] = {
-		spellID = 383017,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] placed!",},
-			},
-			['SPELL_AURA_REMOVED'] = {
-				messages = {"[LINK] finished!",},
-			},
-		},
-	},
-    ['earthbindTotem'] = {
+	['earthbindTotem'] = {
 		spellID = 2484,
 		configDisplay = {
 			isDefault = true,
@@ -308,6 +211,21 @@ local defaults = {
 			},
 			-- TODO save GUID of SPELL_SUMMON, and match this to that.
 			['UNIT_DIED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['earthElemental'] = {
+		spellID = 198103,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
 				messages = {"[LINK] finished!",},
 			},
 		},
@@ -356,6 +274,51 @@ local defaults = {
 		events = {
 			['SPELL_CAST_SUCCESS'] = {
 				messages = {"[LINK] activated!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['earthGrabTotem'] = {
+		spellID = 51485,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] placed!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['groundingTotem'] = {
+		spellID = 204336,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] placed!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['healingTideTotem'] = {
+		spellID = 108280,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] placed!",},
 			},
 			['SPELL_AURA_REMOVED'] = {
 				messages = {"[LINK] finished!",},
@@ -411,6 +374,28 @@ local defaults = {
 			},
 		},
 	},
+	['poisonCleansingTotem'] = {
+		spellID = 383013,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] placed!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+			['SPELL_DISPEL'] = {
+				messages = {"Cleansed [TARGET]'s [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					EXTRA = true,
+				},
+			},
+		},
+	},
 	['purge'] = {
 		spellID = 370,
 		throttle = 0.25,
@@ -447,50 +432,8 @@ local defaults = {
 			},
 		},
 	},
-	['thunderstorm'] = {
-		spellID = 51490,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] activated!",},
-			},
-		},
-	},
-	['windShear'] = {
-		spellID = 57994,
-		configDisplay = {
-			isDefault = true,
-			disabledChannels = {whisper = true},
-		},
-		events = {
-			['SPELL_INTERRUPT'] = {
-				messages = {"Interrupted [TARGET]'s [EXTRALINK]!",},
-				tags = {
-					TARGET = true,
-					EXTRA = true, -- Replaces AURA and TARSPELL.
-				},
-			},
-			['SPELL_MISSED'] = {
-				messages = {"[LINK] [MISSTYPE] [TARGET]!",},
-				tags = {
-					TARGET = true,
-					MISSTYPE = true,
-				},
-			},
-			['RSA_SPELL_IMMUNE'] = { -- Fake event to easily generate options for immune specific messages.
-				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
-				tags = {
-					TARGET = true,
-					MISSTYPE = true,
-				},
-			},
-		},
-	},
-    ['tranquilAirTotem'] = {
-		spellID = 383019,
+	['spiritLinkTotem'] = {
+		spellID = 98008,
 		configDisplay = {
 			isDefault = true,
 			disabledChannels = {whisper = true},
@@ -501,6 +444,48 @@ local defaults = {
 			},
 			['SPELL_AURA_REMOVED'] = {
 				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['stoneBulwarkTotem'] = {
+		spellID = 108270,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] placed!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['surgingTotem'] = {
+		spellID = 444995,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] placed!",},
+			},
+			['SPELL_AURA_REMOVED'] = {
+				messages = {"[LINK] finished!",},
+			},
+		},
+	},
+	['thunderstorm'] = {
+		spellID = 51490,
+		configDisplay = {
+			isDefault = true,
+			disabledChannels = {whisper = true},
+		},
+		events = {
+			['SPELL_CAST_SUCCESS'] = {
+				messages = {"[LINK] activated!",},
 			},
 		},
 	},
@@ -534,22 +519,36 @@ local defaults = {
 			},
 		},
 	},
-	['surgingTotem'] = {
-		spellID = 444995,
+	['windShear'] = {
+		spellID = 57994,
 		configDisplay = {
 			isDefault = true,
 			disabledChannels = {whisper = true},
 		},
 		events = {
-			['SPELL_CAST_SUCCESS'] = {
-				messages = {"[LINK] placed!",},
+			['SPELL_INTERRUPT'] = {
+				messages = {"Interrupted [TARGET]'s [EXTRALINK]!",},
+				tags = {
+					TARGET = true,
+					EXTRA = true, -- Replaces AURA and TARSPELL.
+				},
 			},
-			['SPELL_AURA_REMOVED'] = {
-				messages = {"[LINK] finished!",},
+			['SPELL_MISSED'] = {
+				messages = {"[LINK] [MISSTYPE] [TARGET]!",},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
+			},
+			['RSA_SPELL_IMMUNE'] = { -- Fake event to easily generate options for immune specific messages.
+				messages = {"[TARGET] [MISSTYPE] [LINK]!"},
+				tags = {
+					TARGET = true,
+					MISSTYPE = true,
+				},
 			},
 		},
 	},
-
 }
 
 RSA.monitorData.shaman, RSA.configData.shaman = RSA.PrepareDataTables(defaults)
